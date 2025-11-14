8100066f6549259b4411ed6a1415cbe16a06ee1d
hyoee <jhk2929616@gmail.com>
2025-11-15 00:20:55 +0900

    Update dags_bash_operator

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -7,7 +7,7 @@ from airflow.sdk import DAG, chain
 with DAG(
     dag_id="dags_bash_operator", # 파이썬 파일명과 상관 없음, 일반적으로 파일명과 dags 일치시키는 것이 좋음
     schedule="0 0 * * *", # 분, 시, 일, 월, 요일 -> 매일 0시 0분마다 돈다
-    start_date=pendulum.datetime(2025, 11, 14, tz="Asial/Seoul"), # dag이 언제부터 돌것인지
+    start_date=pendulum.datetime(2025, 11, 14, tz="Asia/Seoul"), # dag이 언제부터 돌것인지
     catchup=False, # 돌린시점과 시작 시점 사이에 누락된 구간을 돌리지 않음, 일반적으로 False
     # dagrun_timeout= datetime.timedelta(minutes=60), # 60분 이상 돌면 실패
     # tags=["example", "example2", "example3"],
