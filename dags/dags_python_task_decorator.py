04fb008e8cbcbc3386462230c1fa38953ad8e076
hyoee <jhk2929616@gmail.com>
2025-11-16 20:02:46 +0900

    airflow 3.0 python decorator 오류 수정

--- dags/dags_python_task_decorator.py
+++ dags/dags_python_task_decorator.py
@@ -1,53 +1,18 @@
 import pendulum
 from airflow.sdk import dag, task
 
-
-# dag_id는 반드시 유니크하게 설정하세요.
 @dag(
-    dag_id='hyokyung_taskflow_test_v1',   # ✔ 충돌 방지용 고유 이름
-    schedule='0 2 * * 1',                 # 매주 월요일 02:00
-    start_date=pendulum.datetime(2025, 11, 14, tz='Asia/Seoul'),
+    schedule='0 2 * * *',
+    start_date=pendulum.datetime(2025, 1, 1, tz='Asia/Seoul'),
     catchup=False
 )
-def hyokyung_taskflow_test():
+def dags_python_task_decorator():
 
-    # TaskFlow 기반 Task
-    @task(task_id="print_context")
-    def print_context(some_input: str):
+    @task(task_id="print_the_context")
+    def print_context(some_input):
         print(some_input)
-        return "task finished"
+        return "Whatever you return gets printed in the logs"
 
-    # Task 생성 (호출 = 실행 X / Task 객체 생성)
-    print_context("taskflow decorator 테스트 중입니다.")
+    print_context('task_decorator 설명')
 
-
-# DAG 인스턴스 생성 (이 줄 없으면 DAG 등록 안 됨)
-example = hyokyung_taskflow_test()
-
-
-# from airflow.decorators import task
-
-# with DAG(
-#     dag_id = 'dags_python_task_decorator',
-#     schedule='0 2 * * 1',
-#     start_date=pendulum.datetime(2025, 11, 14, tz='Asia/Seoul'),
-#     catchup=False
-# ) as dag:
-    
-#     @task(task_id = 'python_task_1')
-#     def print_context(some_input):
-#         print(some_input)
-#     python_task_1 = print_context('task_decorator 설명')
-
-
-
-
-
-# @task
-# def print_context(some_input):
-#     print(some_input)
-# with DAG(dag_id = 'dags_python_task_decorator',
-#          schedule='0 2 * * 1',
-#          start_date=pendulum.datetime(2025, 11, 14, tz='Asia/Seoul'),
-#          catchup=False):
-#     python_task_1 = print_context("task_decorator 실행")
+example_dag = dags_python_task_decorator()
