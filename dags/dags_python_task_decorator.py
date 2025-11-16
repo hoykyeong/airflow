1559972943ca66de146349dc88cdbc6bbf898184
hyoee <jhk2929616@gmail.com>
2025-11-16 17:50:07 +0900

    decorator airflow 3.0 반영

--- dags/dags_python_task_decorator.py
+++ dags/dags_python_task_decorator.py
@@ -20,17 +20,26 @@
 # example = dags_python_task_decorator()
 
 import pendulum
-from airflow.sdk import DAG
-from airflow.decorators import task
+from airflow.sdk import DAG, task
+# from airflow.decorators import task
 
-with DAG(
-    dag_id = 'dags_python_task_decorator',
-    schedule='0 2 * * 1',
-    start_date=pendulum.datetime(2025, 11, 14, tz='Asia/Seoul'),
-    catchup=False
-) as dag:
+# with DAG(
+#     dag_id = 'dags_python_task_decorator',
+#     schedule='0 2 * * 1',
+#     start_date=pendulum.datetime(2025, 11, 14, tz='Asia/Seoul'),
+#     catchup=False
+# ) as dag:
     
-    @task(task_id = 'python_task_1')
-    def print_context(some_input):
-        print(some_input)
-    python_task_1 = print_context('task_decorator 설명')
+#     @task(task_id = 'python_task_1')
+#     def print_context(some_input):
+#         print(some_input)
+#     python_task_1 = print_context('task_decorator 설명')
+
+@task
+def print_context(some_input):
+    print(some_input)
+with DAG(dag_id = 'dags_python_task_decorator',
+         schedule='0 2 * * 1',
+         start_date=pendulum.datetime(2025, 11, 14, tz='Asia/Seoul'),
+         catchup=False):
+    print_context()
