193844d75116abfcd57bad5b7791e1582368fdf1
hyoee <jhk2929616@gmail.com>
2025-11-16 17:53:33 +0900

    decorator airflow 3.0 반영

--- dags/dags_python_task_decorator.py
+++ dags/dags_python_task_decorator.py
@@ -1,26 +1,26 @@
-# import pendulum
-# from airflow.sdk import dag, task
+import pendulum
+from airflow.sdk import dag, task
 
-
-# @dag(
-#     dag_id = 'dags_python_task_decorator',
-#     schedule='0 2 * * 1', # 매주 월요일 2시 0분에 도는 스케줄
-#     # @once, @daliy, @weelky, @month;y, @yearly
-#     start_date=pendulum.datetime(2025, 11, 14, tz='Asia/Seoul'),
-#     catchup=False
-# )
-# def dags_python_task_decorator():
-
-#     @task(task_id="python_task_1") # task 데커레이터, 인자는 파이썬 오퍼레이터에서의 주도한 task_id랑 같은 파라미터
-#     def print_context(some_input):
-#         print(some_input)
-
-#     python_task_1 = print_context("task_decorator 실행")
-
-# example = dags_python_task_decorator()
 
 import pendulum
-from airflow.sdk import DAG, task
+from airflow.sdk import dag, task
+
+@dag(
+    dag_id='dags_python_task_decorator',
+    schedule='0 2 * * 1',
+    start_date=pendulum.datetime(2025, 11, 14, tz='Asia/Seoul'),
+    catchup=False
+)
+def dags_python_task_decorator():
+
+    @task
+    def print_context(some_input):
+        print(some_input)
+
+    print_context("task_decorator 실행")
+
+example = dags_python_task_decorator()
+
 # from airflow.decorators import task
 
 # with DAG(
@@ -35,11 +35,15 @@ from airflow.sdk import DAG, task
 #         print(some_input)
 #     python_task_1 = print_context('task_decorator 설명')
 
-@task
-def print_context(some_input):
-    print(some_input)
-with DAG(dag_id = 'dags_python_task_decorator',
-         schedule='0 2 * * 1',
-         start_date=pendulum.datetime(2025, 11, 14, tz='Asia/Seoul'),
-         catchup=False):
-    python_task_1 = print_context("task_decorator 실행")
+
+
+
+
+# @task
+# def print_context(some_input):
+#     print(some_input)
+# with DAG(dag_id = 'dags_python_task_decorator',
+#          schedule='0 2 * * 1',
+#          start_date=pendulum.datetime(2025, 11, 14, tz='Asia/Seoul'),
+#          catchup=False):
+#     python_task_1 = print_context("task_decorator 실행")
