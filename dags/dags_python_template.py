874bdab344e063d91834595bb4e1149599febbba
hyoee <jhk2929616@gmail.com>
2025-11-17 22:21:04 +0900

    pythonoperator template 오류

--- dags/dags_python_template.py
+++ dags/dags_python_template.py
@@ -1,7 +1,6 @@
-from airflow.sdk import DAG
+from airflow.sdk import DAG, task
 import pendulum
 from airflow.providers.standard.operators.python import PythonOperator
-from airflow.decorators import task
 
 with DAG(
     dag_id = 'dags_python_template',
@@ -9,26 +8,20 @@ with DAG(
     schedule = '30 9 * * *',
     catchup=False
 ) as dag:
+
     def python_function1(start_date, end_date, **kwargs):
         print(start_date)
         print(end_date)
-    
-    # python_t1 = PythonOperator(
-    #     task_id = 'python_t1',
-    #     python_callable= python_function1,
-    #     op_kwargs={
-    #         'start_date': '{{ data_interval_start | ds }}',
-    #         'end_date': '{{ data_interval_end | ds }}' }
-    # )
+
     python_t1 = PythonOperator(
         task_id='python_t1',
         python_callable=python_function1,
-        templates_dict={
-            "start_date": "{{ data_interval_start }}",
-            "end_date": "{{ data_interval_end }}"
-        },
-        op_kwargs={'start_date': None, 'end_date': None},
+        op_kwargs={
+            "start_date": "{{ data_interval_start | ds}}",
+            "end_date": "{{ data_interval_end | ds}}"
+        }
     )
+
     @task(task_id="python_t2")
     def python_function2(**kwargs):
         print(kwargs)
