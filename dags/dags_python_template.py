914916a6dcad01706ec820bf95601cfedd5c1c95
hyoee <jhk2929616@gmail.com>
2025-11-17 22:03:13 +0900

    import 오타 수정

--- dags/dags_python_template.py
+++ dags/dags_python_template.py
@@ -14,7 +14,7 @@ with DAG(
         print(end_date)
     
     python_t1 = PythonOperator(
-        task_id = 'python_t`',
+        task_id = 'python_t1',
         python_callable= python_function1,
         op_kwargs={'start_date' : '{{date_interval_start | ds}}',
                     'end_date' : '{{date_interval_end | ds}}'
