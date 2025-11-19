19a88eee0c00349dfd79be29313d089375dcf4fc
hyoee <jhk2929616@gmail.com>
2025-11-19 20:34:01 +0900

    오타 수정

--- dags/dags_bash_with_xcom.py
+++ dags/dags_bash_with_xcom.py
@@ -11,9 +11,9 @@ with DAG(
     bash_push = BashOperator(
         task_id = 'bash_puth',
         bash_command="echo START && "
-                        "echo XCOM_PYSHED "
-                        "{{ti.xcom_push(key='bash_pushed', value='first_bash_message') }} && "
-                        "echo COMPLETE"
+                 "echo XCOM_PUSHED "
+                 "{{ ti.xcom_push(key='bash_pushed',value='first_bash_message') }} && "
+                 "echo COMPLETE"
     )
     bash_pull = BashOperator(
         task_id = 'bash_pull',
@@ -21,7 +21,7 @@ with DAG(
             'PUSHED_VALUE' : "{{ ti.xcom_pull(key='bash_pused', task_ids='bash_push') }}",
             'RETURN_VALUE' : "{{ ti.xcom_pull(task_ids = 'bash_push') }}"
         },
-        bash_command= "echo $PUSHED_VALUE && echo $RETURN_VALUE",
+        bash_command="echo $PUSHED_VALUE && echo $RETURN_VALUE ",
         do_xcom_push=False
     )
     bash_push >> bash_pull
