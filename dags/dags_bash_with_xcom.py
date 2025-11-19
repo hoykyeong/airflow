624e8dbc9b17e9b3b05ae959e2f7d6084ed8785e
hyoee <jhk2929616@gmail.com>
2025-11-19 20:22:33 +0900

    오타 수정

--- dags/dags_bash_with_xcom.py
+++ dags/dags_bash_with_xcom.py
@@ -21,7 +21,7 @@ with DAG(
             'PUSHED_VALUE' : "{{ ti.xcom_pull(key='bash_pused', task_idx='bash_push') }}",
             'RETURN_VALUE' : "{{ ti.xcom_pull(task_ids = 'bash_push') }}"
         },
-        bash_command= "echo $PUSHED_VALUE && echo $RETRUN_VALUE",
+        bash_command= "echo $PUSHED_VALUE && echo $RETURN_VALUE",
         do_xcom_push=False
     )
     bash_push >> bash_pull
